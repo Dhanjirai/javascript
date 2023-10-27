2d5c48b580444a896b735138ea7b92abe023ac7b
Dhanjirai <148879636+Dhanjirai@users.noreply.github.com>
2023-10-27 14:37:13 +0000

    variable in javascript

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456;
+let Email  = "dhanjitrai@gmail.com";
+var accountPasward = 147258
+city = "Jaipur ";
+
+// accountId = 789456;
+// console.log(accountId) we can not change the constant variable in javascript 
+
+Email = "abcd@gmail.com";
+accountPasward = 963852;
+city = "varanasi";
+console.table([accountId,Email,accountPasward,city])
+/*
+we are not prefer to use a var variable in a javascript becouse their is problem in javascript with block 
+var is not able to understand the concept of block in javascript 
+*/
