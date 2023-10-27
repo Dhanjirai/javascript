fea73f46e3704be68f907412953ca1420efab383
Dhanjirai <148879636+Dhanjirai@users.noreply.github.com>
2023-10-27 14:37:34 +0000

    datatype and conversion

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+// conversion
+let score = "33abc"
+console.log(typeof score)
+let score_in_int = Number (score)
+console.log(typeof (score_in_int))
+console.log(score_in_int)
+/*
+if "33"=> 33 "33" is a string then they can easily can be converted into a number 
+"33abc"=> this type is a number but the output is NaN (not a number )
+true => 1; false => 0;
+*/
